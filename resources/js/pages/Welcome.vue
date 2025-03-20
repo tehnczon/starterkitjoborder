ceed8fce4817ca353a0ace6015cbb6e75b45a7cd
tehnczon <tehnczonlanticse@gmail.com>
2025-03-20 15:47:08 +0800

    scond commit

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+        <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
+            <nav class="flex items-center justify-end gap-4">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >
+                    Dashboard
+                </Link>
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                    >
+                        Log in
+                    </Link>
+                    <Link
+                        :href="route('register')"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    >
+                        Register
+                    </Link>
+                </template>
+            </nav>
+        </header>
+        <div class="duration-750 starting:opacity-0 flex w-full items-center justify-center opacity-100 transition-opacity lg:grow">
+            <main class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row">
+                <div
+                    class="flex-1 rounded-bl-lg rounded-br-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:rounded-br-none lg:rounded-tl-lg lg:p-20"
+                ><div class="flex flex-col text-center margin-auto">
+    <h1 class="text-4xl font-bold text-red-700 margin-auto">Welcome to RJA</h1>
+    <p class="mt-2">RJA is a web application that allows you to manage your projects and tasks.</p>
+</div>
+
+                    <div class="flex-1 flex items-center justify-center lg:rounded-tr-lg lg:rounded-bl-none lg:rounded-tl-lg lg:rounded-br-lg">
+                    <img src="../../../img/rjalogo.png" >
+                </div>
+                </div>
+
+
+            </main>
+        </div>
+        <div class="h-14.5 hidden lg:block"></div>
+    </div>
+</template>
